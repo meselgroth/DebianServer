840123be83b40311ecae088097c307367acb728a
meselgroth <meselgroth@yahoo.com>
2014-01-28 23:32:23 +0000

    Call to gpio to toggle led on 1

--- RaspberryPiMorseCode/RaspberryPiMorseCode/GpioCaller.cs
+++ RaspberryPiMorseCode/RaspberryPiMorseCode/GpioCaller.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿using Raspberry.IO.GeneralPurpose;
+using System;
 
 namespace RaspberryPiMorseCode
 {
     public class GpioCaller
     {
-
         public void Light(string input)
         {
+            var led1 = ConnectorPin.P1Pin11.Output();
+            var connection = new GpioConnection(led1);
+
             if (input=="1")
             {
                 Console.WriteLine("...turning light on");
+                connection.Toggle(led1);
             }
             if (input == "0")
             {
                 Console.WriteLine("...turning light off");
             }
+            connection.Close();
         }
     }
 }
