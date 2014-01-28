ef5d0e683328f4f372898acbfa860a0767479f88
meselgroth <michael.eselgroth@hotels4u.com>
2014-01-28 15:30:09 +0000

    Commands for install git and clone this repo

--- installmono.sh
+++ installmono.sh
@@ -1,2 +1,13 @@
+#if putty
+stty sane
+export TERM=linux
+
+sudo apt-get install git;
+mkdir code
+cd code
+mkdir debianserver
+cd debianserver
+git clone https://github.com/meselgroth/debianserver.git
+
 sudo apt-get update; sudo apt-get upgrade;
 sudo apt-get install mono-complete;
