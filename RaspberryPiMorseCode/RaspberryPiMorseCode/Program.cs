0763bebcce5cbcb4ce3b8112994fdaa8a63c1704
meselgroth <michael.eselgroth@hotels4u.com>
2014-01-29 15:14:15 +0000

    Adding WebApi on self hosting OWIN web server

--- RaspberryPiMorseCode/RaspberryPiMorseCode/Program.cs
+++ RaspberryPiMorseCode/RaspberryPiMorseCode/Program.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
 
 namespace RaspberryPiMorseCode
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var input = string.Empty;
+            Console.WriteLine("Welcome to morse code");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Type w to run Restful Webserver");
+            Console.WriteLine("Type c to run Command Line Interface");
+            Console.WriteLine("Type x to run webserver");
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "w":
+                    RunWebserver();
+                    break;
+                case "c":
+                    RunCLI();
+                    break;
+            }
+        }
+
+        private static void RunWebserver()
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+
+            Console.ReadLine(); 
+        }
+
+        private static void RunCLI()
         {
             var input = string.Empty;
             Console.WriteLine("Welcome to morse code");
@@ -18,9 +60,9 @@ namespace RaspberryPiMorseCode
                 while (input != "x")
                 {
                     input = Console.ReadLine();
-                    
+
                     gpioCaller.Light(input);
-                } 
+                }
             }
         }
     }
